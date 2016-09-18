09a5eef0639f0208fce664f185ac0373280a9fed
antpire <antpire@llorz.com>
2016-09-18 16:21:49 +0800

    Site updated: 2016-09-18 16:21:48

--- js/src/schemes/pisces.js
+++ js/src/schemes/pisces.js
@@ -1,3 +1,3 @@
-// build time:Sun Sep 18 2016 16:11:09 GMT+0800 (中国标准时间)
+// build time:Sun Sep 18 2016 16:21:47 GMT+0800 (中国标准时间)
 $(document).ready(function(){var e=$(".header-inner").height()+10;$("#sidebar").css({"margin-top":e}).show()});
 //rebuild by neat 
